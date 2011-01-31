9d42118433f4319b8233747c19ecd498579f7239
TJ Usiyan <griotspeak@gmail.com>
2011-01-31 15:36:55 +0000

    Monome Update

--- gs.pChange/trunk/gs.pChange.js
+++ gs.pChange/trunk/gs.pChange.js
@@ -319,7 +319,7 @@ function Monome(aColumns, aRows, aOutlet) {
 
         this.tempOff = function() {
             tempState = 0;
-            that.ledFunction(mCol, mRow, actualState);
+            that.ledFunction(mCol, mRow, tempState);
         };
     }
 
