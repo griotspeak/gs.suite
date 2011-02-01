5ca5ce4a8f7b26cc0fab81eff4b55a4db55c9738
TJ Usiyan <griotspeak@gmail.com>
2011-02-01 17:49:14 +0000

    Monome Update

--- gs.pChange/trunk/gs.pChange.js
+++ gs.pChange/trunk/gs.pChange.js
@@ -1,3 +1,5 @@
+//<License
+
 /*
 -*- coding: utf-8 -*-
 gs.pChange v00.000
@@ -25,6 +27,7 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+//License>
 
 var autowatch = 1;
 
@@ -398,6 +401,15 @@ function Monome(aColumns, aRows, aOutlet) {
         if (gDebugItem.functionName) { post("    --window :", aMethodToInvoke, "--\n"); }
         var iColumn;
     	var iRow;    
+
+        if (gDebugItem.functionArguments) {
+            post("mColumns", mColumns, "\n");
+            post("mRows", mRows, "\n");
+            post("aLeftColumn", aLeftColumn, "\n");
+            post("aRightColumn", aRightColumn, "\n");
+            post("aTopRow", aTopRow, "\n");
+            post("aBottomRow", aBottomRow, "\n");
+        }
 
         for (iColumn = aLeftColumn; iColumn < aRightColumn; iColumn++) {
             for (iRow = aTopRow; iRow < aBottomRow; iRow++) {
